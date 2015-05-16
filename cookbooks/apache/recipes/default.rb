e102b2ca3307b946223b4e54db61001b953e5bb0
Obaid AShfaq <obaid.ashfaq@rackspace.co.uk>
2015-05-16 09:35:52 +0100

    install-apache,start,mkae-homepage

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#install apache
+package "apache2" do
+	action :install
+end
+
+#start the service
+
+#make sure that it starts at reboot
+service "apache2" do
+	action [:start, :enable]
+end
+
+#write homepage
+cookbook_file "/var/www/index.html" do
+	source "index.html"
+	mode "0644"
+end
